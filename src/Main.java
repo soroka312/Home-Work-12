db635ba8c6db2887f78529a517cbcd458e864865
Ilea <sorokin58473@gmail.com>
2024-04-14 00:14:42 +1000

    Сделал

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,26 @@ public class Main {
         Author author1 = new Author("Маргарет", "Митчелл");
         Book book1 = new Book("Унесённые ветром", author1, 1936);
 
+        System.out.println("Имя автора: " + author1.getName());
+        System.out.println("Фамилия автора: " + author1.getSurname());
+
+        System.out.println("Название: " + book1.getTitle());
+        System.out.println("Автор: " + book1.getAuthor());
+        System.out.println("Год: " + book1.getYear());
+
+
         System.out.println(book1);
 
         Author author2 = new Author("Анджей", "Сапковский");
         Book book2 = new Book("Последнее желание", author2, 1996);
         book2.setYear(1986);
+
+        System.out.println("Имя автора: " + author2.getName());
+        System.out.println("Фамилия автора: " + author2.getSurname());
+
+        System.out.println("Название: " + book2.getTitle());
+        System.out.println("Автор: " + book2.getAuthor());
+        System.out.println("Год: " + book2.getYear());
 
         System.out.println(book2);
     }
