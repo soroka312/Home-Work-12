dc951d741184c7f00a4bda5521bcb7cf6199b5d6
Ilea <sorokin58473@gmail.com>
2024-04-13 23:26:10 +1000

    Сделал

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,32 @@
 public class Book {
+    private String title;
+    private Author author;
+    private int year;
+
+    public Book(String title, Author author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Название: " + title + " / Автор: " + author + " / Год: " + year;
+    }
 }
